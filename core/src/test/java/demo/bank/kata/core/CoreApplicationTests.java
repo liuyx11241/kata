ddf4244ab7b9e9eb45679667efff5377f1238567
Yanxi LIU <liuyx11241@users.noreply.github.com>
2018-12-12 13:35:29 +0100

    Scenario Tests

--- core/src/test/java/demo/bank/kata/core/CoreApplicationTests.java
+++ core/src/test/java/demo/bank/kata/core/CoreApplicationTests.java
@@ -1,7 +1,13 @@
 package demo.bank.kata.core;
 
+import demo.bank.kata.dto.AccountDto;
+import demo.bank.kata.exception.AccountNotFoundException;
+import demo.bank.kata.exception.BalanceNotEnoughException;
+import demo.bank.kata.manager.HistoryItemManager;
+import demo.bank.kata.manager.OperationManager;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +15,67 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class CoreApplicationTests {
 
+    @Autowired
+    OperationManager operationManager;
+
+    @Autowired
+    HistoryItemManager historyItemManager;
+
     @Test
     public void contextLoads() {
     }
 
+    @Test
+    public void scenario1() throws InterruptedException {
+        try {
+            AccountDto account = operationManager.deposit("123456", 20.43);
+            Thread.sleep(1000);
+            operationManager.withdraw("123456", 10);
+            Thread.sleep(1000);
+            operationManager.deposit("123456", 20);
+
+            System.out.println();
+            System.out.println();
+            System.out.println(String.format("%-32s|%32s|%16s|%16s", "Time", "Account Number", "Operation", "Balance"));
+            historyItemManager.findByAccount(account.getIdAccount()).forEach(System.out::println);
+        } catch (AccountNotFoundException e) {
+            e.printStackTrace();
+        } catch (BalanceNotEnoughException e) {
+            System.err.println(String.format("Balance Not Enough %s", e.getMessage()));
+        }
+    }
+
+    @Test
+    public void scenario2() {
+        try {
+            AccountDto account = operationManager.deposit("321654", 20.43);
+            operationManager.withdraw("321654", 10);
+            operationManager.deposit("321654", 20);
+            System.out.println();
+            System.out.println();
+            System.out.println(String.format("%-32s|%32s|%16s|%16s|%16s|%16s", "Time", "Account Number", "Type", "Comments", "Operation", "Balance"));
+            historyItemManager.findByAccount(account.getIdAccount()).forEach(System.out::println);
+        } catch (AccountNotFoundException e) {
+            System.err.println(String.format("Account Not Found %s", e.getMessage()));
+        } catch (BalanceNotEnoughException e) {
+            System.err.println(String.format("Balance Not Enough %s", e.getMessage()));
+        }
+    }
+
+    @Test
+    public void scenario3() {
+        try {
+            AccountDto account = operationManager.deposit("123456", 20.43);
+            operationManager.withdraw("123456", 30);
+            operationManager.deposit("123456", 20);
+            System.out.println();
+            System.out.println();
+            System.out.println(String.format("%-32s|%32s|%16s|%16s|%16s|%16s", "Time", "Account Number", "Type", "Comments", "Operation", "Balance"));
+            historyItemManager.findByAccount(account.getIdAccount()).forEach(System.out::println);
+        } catch (AccountNotFoundException e) {
+            System.err.println(String.format("Account Not Found %s", e.getMessage()));
+        } catch (BalanceNotEnoughException e) {
+            System.err.println(String.format("Balance Not Enough %s", e.getMessage()));
+        }
+    }
 }
